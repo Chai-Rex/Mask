288583ed522ce2da1cfc1f32e7e6ba83f7d4190b
GrbTheHrmit <ggrb43@gmail.com>
2026-01-29 18:24:04 -0700

    hooking pickups to blackboard

--- Mask/Assets/_Project/Scripts/Interactables/InteractablePickup.cs
+++ Mask/Assets/_Project/Scripts/Interactables/InteractablePickup.cs
@@ -5,12 +5,17 @@ public class PickupableItem : MonoBehaviour, IInteractable {
     [SerializeField] private string _iItemName = "Item";
     [SerializeField] private string _iPickupVerb = "Pickup";
 
+    private string _stateString;
+
     private Rigidbody _rigidbody;
 
     public string InteractionVerb => _iPickupVerb;
 
     private void Awake() {
         _rigidbody = GetComponent<Rigidbody>();
+
+        _stateString = "is" + _iItemName + "PickedUp";
+        StoryStateSO.RegisterInitialState(new StateVariable(_stateString, false, false));
     }
 
     public async void OnInteract(GameObject interactor) {
@@ -29,5 +34,10 @@ public class PickupableItem : MonoBehaviour, IInteractable {
 
     public Transform GetTransform() {
         return transform;
+    }
+
+    public void SetItemPickedUp(bool i_isPickedUp)
+    {
+        StoryStateSO.Instance.SetValue(_stateString, i_isPickedUp);
     }
 }
