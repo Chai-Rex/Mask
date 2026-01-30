c1f3f20eba6d78549bb94c486f0b77737eb37673
GrbTheHrmit <ggrb43@gmail.com>
2026-01-29 22:46:27 -0700

    adding sounds to events

--- Mask/Assets/_Project/Scripts/Events/BaseTimeEvent.cs
+++ Mask/Assets/_Project/Scripts/Events/BaseTimeEvent.cs
@@ -1,9 +1,20 @@
+using AudioSystem;
 using UnityEngine;
 
 public class BaseTimeEvent : MonoBehaviour
 {
+    [SerializeField]
+    private SoundData _eventAudioData;
+
     protected virtual void ActivateTimeEvent()
     {
+        if(_eventAudioData != null && _eventAudioData.Clip != null)
+        {
+            SoundManager.Instance.CreateSound()
+                .WithPosition(gameObject.transform.position)
+                .Play(_eventAudioData);
+        }
 
+       
     }
 }
