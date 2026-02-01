09e3413b0c9621edbccbd661363a5e04109a2e28
GrbTheHrmit <ggrb43@gmail.com>
2026-02-01 07:30:17 -0700

    menu audioooooooooo

--- Mask/Assets/_Project/Scripts/Events/Guitar.cs
+++ Mask/Assets/_Project/Scripts/Events/Guitar.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class Guitar : PickupableItem
 {
+    [SerializeField] RoomSoundEmitter _guitarAudioEmitter;
+
+    private void Start()
+    {
+        if (_guitarAudioEmitter != null)
+        {
+            _guitarAudioEmitter.gameObject.SetActive(false);
+        }
+    }
+
     public override void SetItemPickedUp(bool i_isPickedUp)
     {
         base.SetItemPickedUp(i_isPickedUp);
+
+        if (_guitarAudioEmitter != null)
+        {
+            _guitarAudioEmitter.gameObject.SetActive(i_isPickedUp);
+        }
 
         if (i_isPickedUp)
         {
